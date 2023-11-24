39209b12295c064dee59a759f32ff489eb569185
ShowierData9978 <mellfang36@gmail.com>
2023-11-24 03:19:37 -0600

    Initial Commit

---
+++ src/client/java/xyz/showierdata/civ/CivilizationClient.java
@@ -0,0 +1,10 @@
+package xyz.showierdata.civ;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CivilizationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
